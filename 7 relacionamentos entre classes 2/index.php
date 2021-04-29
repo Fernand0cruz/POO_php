f308ff746a5944957d550a092c851b715b493ac7
Luiz Fernando <cruzluizfernando097@gmail.com>
2021-04-29 15:56:12 -0300

    passado raiva com o prof

--- 7 relacionamentos entre classes 2/index.php
+++ 7 relacionamentos entre classes 2/index.php
@@ -12,14 +12,18 @@
     <pre>
     <?php
     include_once "Lutador.php";
+    include_once "Luta.php";
     $l = array();
 
     $l[0] = new Lutador("Nando", "br", 21, 1.70, 70, 32, 0, 1);
-    $l[1] = new Lutador("Jão bosta", "ch", 53, 2.12, 90, 22, 16, 5);
+    $l[1] = new Lutador("Jão bosta", "ch", 53, 2.12, 70, 22, 16, 5);
 
-    $l[1]->perderLuta();
-    $l[1]->apresentar();
-    $l[1]->status();
+    // $l[0]->apresentar();
+    // $l[1]->apresentar();
+    $UECO1 = new Luta();
+    $UECO1->marcarLuta($l[0] ,$l[1]);
+    $UECO1->lutar();
+
 
     ?>
     </pre>
